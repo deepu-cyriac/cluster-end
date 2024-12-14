daedd38269963eb3dc8cec480ec772b6b3d7c5dc
Deepu Cyriac <deepucvk123@gmail.com>
2024-12-14 18:19:40 +0530

    some changes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+require("dotenv").config();
 
 app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`);
