de2060bd91cc50139b87d97e072b0d58d4327143
Deepu Cyriac <deepucvk123@gmail.com>
2024-12-14 19:00:10 +0530

    Added connection to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 require("dotenv").config();
 
+const run = require("./db/mongo-conn");
+
 app = express();
 const PORT = process.env.PORT || 3001;
+
+app.get("/", async (req, res) => {
+  const db = await run();
+  let products = await db.collection("electronics");
+
+  let results = await products.find({}).limit(50).toArray();
+  res.send(results).status(200);
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`);
